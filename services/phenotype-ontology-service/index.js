78e169e643346f730ff98d328c36c54f4bfa26fa
Sibyl <sibyl@wormbase.org>
2020-03-31 14:04:26 -0400

    fetch phenotype term by ID from search

--- services/phenotype-ontology-service/index.js
+++ services/phenotype-ontology-service/index.js
@@ -1,31 +1,52 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
+const axios = require('axios');
 
-const mockGet = (id) => ({
-  id: 'WBPhenotype:0002254',
-  name: '3 prime target RNA uridylation reduced'
-})
+const getPhenotypeTerm = (id) => {
+  return axios.get('http://wormbase-search-ws275.us-east-1.elasticbeanstalk.com/integration/search-exact', {
+    params: {
+      class: 'phenotype',
+      q: id
+    }
+  }).then(response => {
+    const {id, label: name} = response.data;
+    return {
+      id: id,
+      name: name,
+      type: 'phenotype'
+    };
+  }).catch(error => {
+    console.log(error);
+  })
+}
+
+// ({
+//   id: 'WBPhenotype:0002254',
+//   name: '3 prime target RNA uridylation reduced'
+// })
 
 const typeDefs = gql`
   type Query {
-    get(id: String): PhenotypeOntology
+    get(id: String!): PhenotypeOntology
   }
 
   type PhenotypeOntology @key(fields: "id") {
     id: ID!
     name: String
+    type: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    get() {
-      return mockGet(1);
+    get(parent, args) {
+      const { id } = args;
+      return getPhenotypeTerm(id);
     }
   },
   User: {
     __resolveReference({id}){
-      return mockGet(id)
+      return getPhenotypeTerm(id)
     }
   }
 }
